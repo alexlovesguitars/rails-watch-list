41687d736d3b5d066205a7206dc1baadd042f8a9
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-02-15 20:29:50 +0100

    Refresh

--- db/migrate/20250215191726_add_reference_to_bookmark.rb
+++
@@ -1,6 +0,0 @@
-class AddReferenceToBookmark < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :bookmarks, :movie_id, null: false, foreign_key: true
-    add_reference :bookmarks, :list_id, null: false, foreign_key: true
-  end
-end
